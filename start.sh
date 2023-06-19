a944652e0dd90212202648e1ed3c55820407c272
hemanthbot69 <104711832+hemanthbot69@users.noreply.github.com>
2023-06-19 15:59:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hansaka-Anuhas/AutoFilterBot.git /AutoFilterBot
+  git clone https://github.com/hemanthbot69/MyAutoFilter69.git /MyAutoFilter69
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterBot
+  git clone $UPSTREAM_REPO /MyAutoFilter69
 fi
 cd /AutoFilterBot
 pip3 install -U -r requirements.txt
